427b979ce8d49ae7e639197c3b5c1fad0415d5c4
pure_yu <yuliya.s.belovol@gmail.com>
2019-09-05 20:56:09 +0300

    3rd commit, prints input, output, result

--- script.py
+++ script.py
@@ -1,1 +1,20 @@
-print("Hello, World!")
+"""
+This script takes 1 parameter:
+    * int - some number
+
+Usage example:
+    python script.py <int x>
+
+"""
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('x', type=int)
+args = parser.parse_args()
+
+x = args.x
+print("Input: ", x)
+print("Output: ", int(x)**2)
+print("Result: ", f"{x}*{x}={int(x)**2}")
+
